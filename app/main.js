ad04df81c0109231298d50a99563ea4a814b95fd
Steffen Larson <steffman13@gmail.com>
2021-01-05 14:17:15 -0700

    Connected the Jobs Controller to main.js

--- app/main.js
+++ app/main.js
@@ -1,9 +1,10 @@
 import CarsController from "./Controllers/CarsController.js";
-
+import JobsController from "./Controllers/JobsController.js"
 
 class App {
   //valuesController = new ValuesController();
   carsController = new CarsController()
+  jobsController = new JobsController()
 }
 
 window["app"] = new App();
