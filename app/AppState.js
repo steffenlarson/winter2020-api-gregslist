ddfec2cfebb8edd04d567ab853a338ebb336ec10
Steffen Larson <steffman13@gmail.com>
2021-01-05 13:36:43 -0700

    put job into appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,10 +1,14 @@
 import Car from "./Models/Car.js"
+import Job from "./Models/Job.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
   /** @type {Car[]} */
   cars = []
+
+  /** @type {Job[]} */
+  jobs = []
 }
 
 export const ProxyState = new Proxy(new AppState(), {
